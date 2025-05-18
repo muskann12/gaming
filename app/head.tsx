1108f2ca469bb8f06de9c0bc1b747501b120898d
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2025-05-18 10:34:24 +0500

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,8 @@ export default function Head() {
     <>
       <title>Gaming Store</title>
       <meta name="description" content="Buy the best games and accessories online." />
-      <meta name="google-site-verification" content="<meta name="google-site-verification" content="UoqxQ4fbdL8KvEfOZK6Y7Ztr8lSA3nsPffYlWC8-UlE" />" />
+      <meta name="google-site-verification" content="UoqxQ4fbdL8KvEfOZK6Y7Ztr8lSA3nsPffYlWC8-UlE" />
+
     </>
   );
 }
